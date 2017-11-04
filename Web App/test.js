98ffc5798d721f069468b8e65003483900de07b8
Thrivikram Mudunuri <mthrivikram@gmail.com>
2017-11-05 01:41:09 +0530

    Changed Array representation

--- Web App/test.js
+++ Web App/test.js
@@ -1,8 +1,9 @@
 // Initialize Firebase
 var cLat = ['0'], cLong = ['0']; //Cabs coordinates
 var pLong = ['0'], pLat = ['0']; //Passenger coordinates
-var uluru;
+var uluru, map;
 var cMarkers = ['0'], pMarkers = ['0']; //Maps Markers
+
 var cabIcon = 'http://maps.google.com/mapfiles/kml/paddle/ylw-blank.png'; //Marker Icons
 var psIcon = 'http://maps.gstatic.com/mapfiles/cb/man_arrow-0.png';
 var config = {
@@ -14,138 +15,80 @@ var config = {
   messagingSenderId: "963805026473"
 };
 firebase.initializeApp(config);
-
-//New Markers
 var cabsRef = firebase.database().ref().child('cabs');
-cabsRef.on('value', function(snapshot){
-    cLat[1] = snapshot.child('cb1/lat').val();
-    cLong[1] = snapshot.child('cb1/long').val();
-    cLat[2] = snapshot.child('cb2/lat').val();
-    cLong[2] = snapshot.child('cb2/long').val();
-    cLat[3] = snapshot.child('cb3/lat').val();
-    cLong[3] = snapshot.child('cb3/long').val();
-    cLat[4] = snapshot.child('cb4/lat').val();
-    cLong[4] = snapshot.child('cb4/long').val();
-    cLat[5] = snapshot.child('cb5/lat').val();
-    cLong[5] = snapshot.child('cb5/long').val();
-});
-
 var passengersRef = firebase.database().ref().child('passengers');
-passengersRef.on('value', function(snapshot){
-    pLat[1] = snapshot.child('ps1/lat').val();
-    pLong[1] = snapshot.child('ps1/long').val();
-    pLat[2] = snapshot.child('ps2/lat').val();
-    pLong[2] = snapshot.child('ps2/long').val();
-    pLat[3] = snapshot.child('ps3/lat').val();
-    pLong[3] = snapshot.child('ps3/long').val();
-    pLat[4] = snapshot.child('ps4/lat').val();
-    pLong[4] = snapshot.child('ps4/long').val();
-    pLat[5] = snapshot.child('ps5/lat').val();
-    pLong[5] = snapshot.child('ps5/long').val();
-});
 
 
 $('#get_map').click(function initMap() {
-    uluru = {lat: Number(cLat[1]), lng: Number(cLong[1])};
-    var map = new google.maps.Map(document.getElementById('map'), {
+    uluru = {lat: 13, lng: 77.58};
+    map = new google.maps.Map(document.getElementById('map'), {
       zoom: 12,
       center: uluru
     });
-    cMarkers[1] = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      title: 'Cab 1',
-      label: '1',
-      icon: cabIcon
-    });
-    cMarkers[2] = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      title: 'Cab 2',
-      label: '2',
-      icon: cabIcon
-    });
-
-    cMarkers[3] = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      title: 'Cab 3',
-      label: '3',
-      icon: cabIcon
-    });
-
-    cMarkers[4] = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      title: 'Cab 4',
-      label: '4',
-      icon: cabIcon
-    });
-
-    cMarkers[5] = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      title: 'Cab 5',
-      label: '5',
-      icon: cabIcon
-    });
-
-    setInterval(function(){
-        cMarkers[1].setPosition(new google.maps.LatLng(Number(cLat[1]),Number(cLong[1])));
-        cMarkers[2].setPosition(new google.maps.LatLng(Number(cLat[2]),Number(cLong[2])));
-        cMarkers[3].setPosition(new google.maps.LatLng(Number(cLat[3]),Number(cLong[3])));
-        cMarkers[4].setPosition(new google.maps.LatLng(Number(cLat[4]),Number(cLong[4])));
-        cMarkers[5].setPosition(new google.maps.LatLng(Number(cLat[5]),Number(cLong[5])));
-    }, 2000);
+});
 
 
-//Passengers
+//Add passengers
 
-    pMarkers[1] = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      title: 'Passenger 1',
-      label: '1',
-      icon: psIcon
-    });
-    pMarkers[2] = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      title: 'Passenger 2',
-      label: '2',
-      icon: psIcon
-    });
+$('#add_ps').click(
+function addPassenger() {
+    var pId, lat, long;
+    pId = prompt("Enter Passenger ID");
+    lat = prompt("Enter Latitude");
+    long = prompt("Enter Longitude");
+  firebase.database().ref().child('passengers/ps' + pId).set({
+    lat: lat,
+    long: long
+  });
 
-    pMarkers[3] = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      title: 'Passenger 3',
-      label: '3',
-      icon: psIcon
-    });
+  pMarkers[Number(pId)] =  new google.maps.Marker({
+        position: uluru,
+        map: map,
+        title: 'Passenger '+ pId,
+        label: pId,
+        icon: psIcon
+      });
 
-    pMarkers[4] = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      title: 'Passenger 4',
-      label: '4',
-      icon: psIcon
-    });
+      passengersRef.on('value', function(snapshot){
+          pLat[Number(pId)] = snapshot.child('ps'+ pId +'/lat').val();
+          pLong[Number(pId)] = snapshot.child('ps'+ pId + '/long').val();
+          });
 
-    pMarkers[5] = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      title: 'Passenger 5',
-      label: '5',
-      icon: psIcon
-    });
+          setInterval(function(){
+              pMarkers[Number(pId)].setPosition(new google.maps.LatLng(pLat[Number(pId)],pLong[Number(pId)]));
+          }, 2000);
+});
 
-    setInterval(function(){
-        pMarkers[1].setPosition(new google.maps.LatLng(Number(pLat[1]),Number(pLong[1])));
-        pMarkers[2].setPosition(new google.maps.LatLng(Number(pLat[2]),Number(pLong[2])));
-        pMarkers[3].setPosition(new google.maps.LatLng(Number(pLat[3]),Number(pLong[3])));
-        pMarkers[4].setPosition(new google.maps.LatLng(Number(pLat[4]),Number(pLong[4])));
-        pMarkers[5].setPosition(new google.maps.LatLng(Number(pLat[5]),Number(pLong[5])));
-    }, 2000);
 
+
+
+//Add Cabs
+
+$('#add_cab').click(
+function addCab() {
+    var cId, lat, long;
+    cId = prompt("Enter Cab ID");
+    lat = prompt("Enter Latitude");
+    long = prompt("Enter Longitude");
+  firebase.database().ref().child('cabs/cb' + cId).set({
+    lat: lat,
+    long: long
+  });
+
+  cMarkers[Number(cId)] =  new google.maps.Marker({
+        position: uluru,
+        map: map,
+        title: 'Cab '+ cId,
+        label: cId,
+        icon: cabIcon
+      });
+
+      cabsRef.on('value', function(snapshot){
+          cLat[Number(cId)] = snapshot.child('cb'+ cId +'/lat').val();
+          cLong[Number(cId)] = snapshot.child('cb'+ cId + '/long').val();
+          });
+
+          setInterval(function(){
+              cMarkers[Number(cId)].setPosition(new google.maps.LatLng(cLat[Number(cId)],cLong[Number(cId)]));
+          }, 2000);
 });
