4ce2acc9d3db0390e057da84e449f9dd7d85d314
Adithya Vasisth <adithyasvasisth@gmail.com>
2017-11-30 08:38:39 +0530

    Merge branch 'master' of https://gitlab.com/thrvkrm/Cab_Tracker

---
+++ Web App/js/driverexcel.js
@@ -0,0 +1,69 @@
+var config = {
+  apiKey: "AIzaSyAVNwUXjOKZMl9Oep7dRLRk1Cv-M2fHTsA",
+  authDomain: "linuxbasedtracker.firebaseapp.com",
+  databaseURL: "https://linuxbasedtracker.firebaseio.com",
+  projectId: "linuxbasedtracker",
+  storageBucket: "linuxbasedtracker.appspot.com",
+  messagingSenderId: "963805026473"
+};
+firebase.initializeApp(config);
+var customerRequestRef = firebase.database().ref('customerRequestTest');
+
+var rABS = true;
+var workbook;
+var OUT = document.getElementById('out');
+var HTMLOUT = document.getElementById('htmlout');
+
+(function() {
+  var drop = document.getElementById('drop');
+  if (!drop.addEventListener) return;
+
+  function handleDrop(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    var files = e.dataTransfer.files;
+    var f = files[0];
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var data = e.target.result;
+      if (!rABS) data = new Uint8Array(data);
+      workbook = XLSX.read(data, {
+        type: rABS ? 'binary' : 'array'
+      });
+      // Work on the Excel File Here
+
+      var first_sheet_name = workbook.SheetNames[0];
+      var worksheet = workbook.Sheets[first_sheet_name];
+      console.log("wohoo");
+      HTMLOUT.innerHTML= XLSX.utils.sheet_to_html(worksheet);
+      var output = XLSX.utils.sheet_to_json(worksheet);
+      console.log(output);
+      strout = JSON.stringify(output, 2, 2);
+      OUT.textContent = strout;
+      for (i in output) {
+        console.log("sup");
+        var did = output[i].ID, name = output[i].Name, car = output[i].Car;
+        function writeDriverData(did, name, car) {
+          firebase.database().ref('driverDetailsTest/'+ did).update({
+            dname: name,
+            car: car
+          });
+        }
+        writeDriverData(did, name, car);
+      }
+    };
+
+
+    if (rABS) reader.readAsBinaryString(f);
+    else reader.readAsArrayBuffer(f);
+  }
+
+  function handleDragover(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+  }
+  drop.addEventListener('dragenter', handleDragover, false);
+  drop.addEventListener('dragover', handleDragover, false);
+  drop.addEventListener('drop', handleDrop, false);
+})();
